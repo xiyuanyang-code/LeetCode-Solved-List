1aaf01c9209952437c0248723ac582985f83ce7f
xiyuanyang-code <xiyuan__yang@outlook.com>
2025-11-01 14:10:41 +0800

    feat: Add new algorithms or scripts (64.minimum-path-sum.py)

--- 64.minimum-path-sum.py
+++ 64.minimum-path-sum.py
@@ -3,10 +3,23 @@
 #
 # [64] Minimum Path Sum
 #
+from typing import List
+
 
 # @lc code=start
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        
-# @lc code=end
+        m = len(grid)
+        n = len(grid[0])
+        dp = [[4e7] * n for _ in range(m)]
+        dp[0][0] = grid[0][0]
+        for i in range(m):
+            for j in range(n):
+                if i - 1 >= 0:
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j] + grid[i][j])
+                if j - 1 >= 0:
+                    dp[i][j] = min(dp[i][j], dp[i][j - 1] + grid[i][j])
+        return dp[m-1][n-1]
 
+
+# @lc code=end
