08cbd493a683ba48841dbb69e21f285f7fbc4645
xiyuanyang-code <xiyuan__yang@outlook.com>
2025-11-05 22:52:17 +0800

    feat: Add new algorithms or scripts (97.interleaving-string.py)

---
+++ 97.interleaving-string.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=97 lang=python3
+#
+# [97] Interleaving String
+#
+
+# @lc code=start
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
+        # define dp[i][j]
+        dp = [[False] * (len(s2)+1) for _ in range(len(s1)+1)]
+        dp[0][0] = True
+        for i in range(1, len(s1)+1):
+            if s1[:i] == s3[:i]:
+                dp[i][0] = True
+        for j in range(1, len(s2)+1):
+            if s2[:j] == s3[:j]:
+                dp[0][j] = True
+        for i in range(1, len(s1)+1):
+            for j in range(1, len(s2)+1):
+                if (dp[i-1][j] and s1[i-1] == s3[i+j-1]) or (dp[i][j-1] and s2[j-1] == s3[i+j-1]):
+                    dp[i][j] = True
+        return dp[len(s1)][len(s2)]
+                    
+
+# @lc code=end
+
